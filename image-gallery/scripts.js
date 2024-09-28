612b0a64f8fe54aca738d0643b3821ccc8010f28
Mariya Shusharina <mariya.shushar@gmail.com>
2024-09-28 03:43:41 +0200

    refactor: add scripts file

---
+++ image-gallery/scripts.js
@@ -0,0 +1,14 @@
+
+const box = document.querySelector(".search-box");
+const lupaBtn = document.querySelector(".search-btn");
+// const pupaBtn = document.querySelector(".reset-btn");
+const = document.querySelector(".main");
+
+const url = "https://api.unsplash.com/search/photos";
+
+async function search() {
+	const result = await fetch(url);
+	const data = await result.json();
+	console.log(data);
+}
+search();
