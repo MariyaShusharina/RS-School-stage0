e998237e26fac40fe32fbba1fcb4ae75b6b1373f
Mariya Shusharina <mariya.shushar@gmail.com>
2024-09-28 05:21:47 +0200

    feat: add new and rework existing functions

--- image-gallery/scripts.js
+++ image-gallery/scripts.js
@@ -4,11 +4,26 @@ const lupaBtn = document.querySelector(".search-btn");
 // const pupaBtn = document.querySelector(".reset-btn");
 const main = document.querySelector(".main");
 
+const api = "GuVJqqfGl9C6fsyeYxysRBNNr-DZx0b3FI5w4nJ9wAA";
+let val = document.querySelector(".search-box").value;
+
 const url = "https://api.unsplash.com/search/photos";
+let q = url + "/?query=" + val + "&client_id=" + api + "&tag_mode=all&orientation=landscape&page=2&per_page=18"
 
 async function search() {
-	const result = await fetch(url);
+
+	val = document.querySelector(".search-box").value;
+	q = url + "/?query=" + val + "&client_id=" + api + "&tag_mode=all&orientation=landscape&page=2&per_page=18"
+
+	const result = await fetch(q);
 	const data = await result.json();
+
 	console.log(data);
 }
+
 search();
+
+function keyEnter(event) {
+	if (event.key === "Enter") { search(); }
+
+}
