3a02dd5a22daf11b1c2cb8b433a5e3b63f032545
Mariya Shusharina <mariya.shushar@gmail.com>
2024-09-28 06:39:10 +0200

    feat: add showPhotos function

--- image-gallery/scripts.js
+++ image-gallery/scripts.js
@@ -5,7 +5,8 @@ const lupaBtn = document.querySelector(".search-btn");
 const main = document.querySelector(".main");
 
 const api = "GuVJqqfGl9C6fsyeYxysRBNNr-DZx0b3FI5w4nJ9wAA";
-let val = document.querySelector(".search-box").value;
+let val = "horse";
+val = document.querySelector(".search-box").value;
 
 const url = "https://api.unsplash.com/search/photos";
 let q = url + "/?query=" + val + "&client_id=" + api + "&tag_mode=all&orientation=landscape&page=2&per_page=18"
@@ -19,6 +20,8 @@ async function search() {
 	const data = await result.json();
 
 	console.log(data);
+
+	showPhotos(data);
 }
 
 search();
@@ -26,4 +29,26 @@ search();
 function keyEnter(event) {
 	if (event.key === "Enter") { search(); }
 
+}
+
+function showPhotos(data) {
+
+	main.innerHTML = "";
+
+	for (let i = 0; i < data.length; i++) {
+
+		let imgAlt = data[i].alt_description;
+		let imgSrc = data[i].urls.regular;
+		let aHref = data[i].links.html;
+
+		let link = document.createElement("a");
+		link.setAttribute("href", aHref);
+		link.setAttribute("target", "_blank");
+		main.appendChild(link);
+
+		let pic = document.createElement("img");
+		pic.setAttribute("src", imgSrc);
+		pic.setAttribute("alt", imgAlt);
+		main.lastElementChild.appendChild(pic);
+	}
 }
