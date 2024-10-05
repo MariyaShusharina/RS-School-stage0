8c760c147165e9800ca14a9c82f795a5c41c47ba
Mariya Shusharina <mariya.shushar@gmail.com>
2024-10-05 09:59:00 +0200

    feat: add canvas to page, draw lines on game-field

--- random-game/scripts.js
+++ random-game/scripts.js
@@ -1,2 +1,38 @@
 
-const = ;
+const canvas = document.querySelection(".field");
+const set = canvas.getContext("2d");
+const cells = 20;
+
+const cWidth = canvas.width;
+const cHeight = canvas.height;
+
+let snake;
+let food;
+let is GameOver = false;
+
+drawGrid() {
+	set.lineWidth = 1;
+	set.strokeStyle = "#007755";
+	//set.shadowBlur = 0; // (add if it will appear to be necessary)
+	for (let i = 1; i < cells; i++) {
+		let range = (cWidth / cells) * i;
+		set.beginPath();
+		set.moveTo(range, 0);
+		set.lineTo(range, cHeight);
+		set.stroke();
+
+		set.beginPath();
+		set.moveTo(0, range);
+		set.lineTo(cWidth, range);
+		set.stroke();
+
+		set.closePath();
+	}
+}
+
+function initialise() {
+	drawGrid();
+}
+
+initialise();
+
