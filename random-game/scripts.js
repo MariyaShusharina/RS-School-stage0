a89f9bed3f49993d5590f954d8e58b5f5e4d7118
Mariya Shusharina <mariya.shushar@gmail.com>
2024-10-08 02:40:17 +0200

    fix: change the name of variable to avoid conflicts

--- random-game/scripts.js
+++ random-game/scripts.js
@@ -27,8 +27,8 @@ let isGameOver = false;
 let hue = 185;
 let color = "hsl(" + hue + ", 100%, 50%)";
 
-let lineColor = document.body.style.backgroundColor;
-console.log("lineColor is " + lineColor);
+let lineClr = document.body.style.backgroundColor;
+console.log("lineClr is " + lineClr);
 
 /*
 function sleep(ms) {
@@ -38,7 +38,7 @@ function sleep(ms) {
 async */
 function drawGrid() {
 	set.lineWidth = 1;
-	set.strokeStyle = lineColor;
+	set.strokeStyle = lineClr;
 	//set.shadowBlur = 0; // (add if it will appear to be necessary)
 	for (let i = 1; i < cells; i++) {
 		let range = side * i;
