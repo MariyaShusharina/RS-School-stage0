39b2f9caaa1399458381788e95864688ee56ab80
Mariya Shusharina <mariya.shushar@gmail.com>
2024-10-08 05:04:39 +0200

    fix: adjust position of text on canvas

--- random-game/scripts.js
+++ random-game/scripts.js
@@ -66,7 +66,7 @@ function drawGrid() {
 function drawTexts() {
 	setTxt.fillStyle = "#71c69d";
 	setTxt.font = `50px "Atari Classic"`;
-	setTxt.fillText("Score: ", 3 * side, side);
+	setTxt.fillText("Score: ", side, 2 * side);
 }
 
 function drawSnake() {
